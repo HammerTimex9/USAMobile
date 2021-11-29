51b24e85305527eb7a555334d7bf018866cddad9
Thomas Nourse <thomas.nourse.dev@gmail.com>
2021-11-29 08:59:33 -0600

    add home page

--- website/src/hooks/action.jsx
+++
@@ -1,9 +0,0 @@
-import { useApi } from '../service/api';
-
-export function getDataByCoinID() {
-  return async (id) => {
-    const api = useApi();
-    const result = await api.getCoinData(id);
-    return result;
-  };
-}
