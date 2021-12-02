31e78f4f0367d7c1be93cf18364c6abaf570ef7b
Nadeem yousaf <nsourcecodes@gmail.com>
2021-12-02 20:38:49 +0500

    Working on Expo Project

--- mobileapp/src/components/Bits/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as FromSelect } from "./FromSelect";
-export { default as ToSelect } from "./ToSelect";
-export { default as AmountSelect } from "./AmountSelect";
--- mobileapp/src/components/Common/Forms/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as FieldContainer } from "./FieldContainer";
-export { default as TextField } from "./TextField";
-export { default as TextFieldWithSymbol } from "./TextFieldWithSymbol";
