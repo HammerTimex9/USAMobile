1b3348ebdcb7f52e882c5c98a646ecead886f2df
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2022-02-04 12:55:15 -0500

    Quote working. Spreading 2 others

--- website/src/components/Bits/RequestQuote.jsx
+++ website/src/components/Bits/RequestQuote.jsx
@@ -8,8 +8,6 @@ import { useQuote } from '../../contexts/quoteContext';
 import { useColorMode } from '../../contexts/colorModeContext';
 import { useNetwork } from '../../contexts/networkContext';
 
-const axios = require('axios');
-
 const NATIVE_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 const ONEINCH4_API = 'https://api.1inch.io/v4.0';
 const ENDPOINT = '/quote';
@@ -34,32 +32,32 @@ export const RequestQuote = () => {
     setDialog(
       `Estimating rates to swap ${txAmount} of ${fromTokenSymbol} to ${toTokenSymbol} ... `
     );
-    console.log(
-      'Estimating rates to swap ' +
-        txAmount.toString() +
-        ' of ' +
-        fromTokenSymbol +
-        ' to ' +
-        toTokenSymbol +
-        '... '
-    );
-    const params = {
-      fromTokenAddress: fromTokenAddress || NATIVE_ADDRESS,
-      toTokenAddress: toTokenAddress || NATIVE_ADDRESS,
-      fee: REFERRER_FEE,
-      amount: txAmount.toString(),
-    };
-    axios({
-      method: 'get',
-      url: ENDPOINT,
-      baseURL: ONEINCH4_API + '/' + network.id.toString(),
-      data: params,
+    const baseURL = ONEINCH4_API + '/' + network.id.toString();
+    const url =
+      baseURL +
+      ENDPOINT +
+      '?fromTokenAddress=' +
+      (fromTokenAddress || NATIVE_ADDRESS) +
+      '&toTokenAddress=' +
+      (toTokenAddress || NATIVE_ADDRESS) +
+      '&fee=' +
+      REFERRER_FEE.toString() +
+      '&amount=' +
+      txAmount.toString();
+
+    fetch(url, {
+      method: 'GET',
     })
       .then((response) => {
-        console.log('RequestQuote::handleClick()::axios(response):', response);
+        return response.json();
+      })
+      .then((response) => {
+        console.groupCollapsed('RequestQuote::handleClick()::fetch(response):');
+        console.log('response:', response);
+        console.groupEnd();
         setFetching(false);
         const now = new Date();
-        setQuote(response.data);
+        setQuote(response);
         setDialog(
           'Quote valid as of: ' +
             now.toLocaleTimeString('en-US') +
@@ -68,23 +66,10 @@ export const RequestQuote = () => {
       })
       .catch((error) => {
         console.groupCollapsed('RequestQuote::handleClick()::catch(error):');
+        console.log('Error:', error);
+        console.groupEnd();
         setQuote(null);
         setFetching(false);
-        if (error.response) {
-          setDialog(
-            'Bad 1Inch server response. ' + error.response.data.description
-          );
-          console.log('.params:', params);
-          console.log('.data:', error.response.data);
-          console.log('.status:', error.response.status);
-          console.log('.headers', error.response.headers);
-        } else if (error.request) {
-          setDialog('Network error.  Try again.');
-          console.log('.request:', error.request);
-        } else {
-          setDialog('Quote request error: ', error.message);
-        }
-        console.groupEnd();
       });
   };
 
