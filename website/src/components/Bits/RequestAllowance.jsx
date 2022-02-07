677bd5c26d93a6d76d44d75fe700b9c9dfb2798a
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2022-02-06 19:47:29 -0500

    Allowance Button Stubbed.

--- website/src/components/Bits/RequestAllowance.jsx
+++ website/src/components/Bits/RequestAllowance.jsx
@@ -2,138 +2,180 @@ import { useEffect, useState } from 'react';
 import { Box, FormControl, Tooltip } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
 
+import { useMoralis } from 'react-moralis';
 import { useActions } from '../../contexts/actionsContext';
 import { useExperts } from '../../contexts/expertsContext';
 import { useColorMode } from '../../contexts/colorModeContext';
-import useCheckAllowanceAction from '../../actions/useCheckAllowanceAction';
-import useSetAllowanceAction from '../../actions/useSetAllowanceAction';
-import { useMoralisRawWeb3 } from '../../hooks/useMoralisRawWeb3';
+import { useNetwork } from '../../contexts/networkContext';
+
+const NATIVE_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+const ONEINCH4_API = 'https://api.1inch.io/v4.0/approve';
+const CHECK_ENDPOINT = '/allowance';
+const GENERATE_ENDPOINT = '/transaction';
 
 export const RequestAllowance = () => {
-  const { fromTokenAddress, fromTokenSymbol, toTokenSymbol, txAmount } =
+  const { isAuthenticated, user } = useMoralis();
+  const { fromTokenAddress, fromTokenSymbol, fromToken, txAmount } =
     useActions();
   const { setDialog } = useExperts();
   const { colorMode } = useColorMode();
-  const { buttonText, setButtonText } = useState();
-  const { mode, setMode } = useState('idle');
+  const { network } = useNetwork();
 
-  const { checkAllowance, isChecking, allowance, getError } =
-    useCheckAllowanceAction({
-      fromTokenAddress: fromTokenAddress,
-      amount: txAmount,
-    });
-  const { forgeTx, isForging, Tx, forgeError } = useSetAllowanceAction({
-    fromTokenAddress: fromTokenAddress,
-    amount: txAmount,
-  });
-  // TODO: write this function
-  const { pushTx, confirmations } = useMoralisRawWeb3();
+  const { buttonText, setButtonText } = useState('TradingAllowance');
+
+  const [allowance, setAllowance] = useState(0);
+  const [checking, setChecking] = useState(false);
+
+  const [tradeTxData, setTradeTxData] = useState({});
+  const [generating, setGenerating] = useState(false);
+
+  const [setting, setSetting] = useState(false);
 
   useEffect(() => {
-    if (isChecking & !isForging) {
-      setDialog(`Checking ${fromTokenSymbol} trading allowance...`);
-      setButtonText('Checking allownace.');
-      setMode('checking');
-    } else if (isForging & !isChecking) {
-      setDialog(
-        `Creating command to unlock ${txAmount} of ${fromTokenSymbol} for trade ...`
-      );
-      setButtonText('Creating unlock codes.');
-      setMode('forging');
-    } else if (isChecking & isForging) {
-      setDialog(
-        `Impatientce error while checking allowance ${txAmount} of ${fromTokenSymbol}.`
-      );
-      setDialog('Tx collision...');
-      setMode('error');
-    } else if (txAmount - allowance < 0) {
-      if (typeof Tx == 'undefined') {
-        forgeTx({ fromTokenAddress, txAmount });
-      } else {
-        if (confirmations < 1) {
-          setMode('need2sign');
-        } else {
-          setDialog('Allowance unlocked!');
-          setButtonText('Allowance unlocked!');
-          setMode('signed');
-        }
-      }
-    } else {
-      setDialog(`${txAmount} of ${fromTokenSymbol} allowance unlocked.`);
-      setButtonText('Allowance unlocking...');
-      setMode('unlocking');
+    if (!setting & isAuthenticated) {
+      setChecking(true);
+      setDialog('Checking ' + fromTokenSymbol + ' trading allowance...');
+      const baseURL =
+        ONEINCH4_API + '/' + network.id.toString() + CHECK_ENDPOINT;
+      const url =
+        baseURL +
+        '?tokenAddress=' +
+        (fromTokenAddress || NATIVE_ADDRESS) +
+        '&walletAddress=' +
+        user?.attributes['ethAddress'];
+      fetch(url, {
+        method: 'GET',
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((response) => {
+          console.groupCollapsed('RequestAllowance::useEffect(tokenAddress)');
+          console.log('response:', response);
+          console.groupEnd();
+          setChecking(false);
+          setAllowance(response.allowance);
+          if (allowance > 0) {
+            setDialog(
+              allowance >= txAmount
+                ? 'Found a ' +
+                    allowance / 10 ** fromToken?.decimals +
+                    ' ' +
+                    fromTokenSymbol +
+                    ' trading allowance!'
+                : 'You will need to unlock ' +
+                    (txAmount - allowance) / 10 ** fromToken?.decimals +
+                    ' more ' +
+                    fromTokenSymbol +
+                    ' before you can trade all requested ' +
+                    txAmount / 10 ** fromToken?.decimals +
+                    ' ' +
+                    fromTokenSymbol +
+                    '.'
+            );
+          } else {
+            setDialog(
+              'No ' +
+                fromTokenSymbol +
+                ' trading allowance found.  You will have to unlock your ' +
+                fromTokenSymbol +
+                ' before you can trade it.'
+            );
+          }
+          setButtonText(
+            allowance >= txAmount
+              ? allowance / 10 ** fromToken?.decimals +
+                  ' ' +
+                  fromTokenSymbol +
+                  ' Unlocked!'
+              : 'Unlock all ' +
+                  txAmount / 10 ** fromToken?.decimals +
+                  ' ' +
+                  fromTokenSymbol +
+                  ' to trade!'
+          ).catch((error) => {
+            console.groupCollapsed(
+              'RequestAllowance::useEffect(fromTokenAddress)'
+            );
+            console.log('error:', error);
+            console.groupEnd();
+            setAllowance(null);
+            setChecking(false);
+            setButtonText('Re-Check Tx Allowance!');
+          });
+        });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    isChecking,
-    isForging,
-    txAmount,
-    allowance,
-    Tx,
-    fromTokenAddress,
-    confirmations,
-  ]);
+  }, [fromTokenAddress, isAuthenticated, setting, user?.attributes]);
 
-  useEffect(() => {
-    if (txAmount > 0 || confirmations >= 1) {
-      checkAllowance({ fromTokenAddress, txAmount });
-      setMode('checking');
-    }
-  }, [fromTokenAddress, txAmount, confirmations, checkAllowance, setMode]);
+  const handleClick = () => {
+    setGenerating(true);
+    setDialog(
+      'Generating transaction to unlock a ' +
+        txAmount / 10 ** fromToken.decimals +
+        ' ' +
+        fromTokenSymbol +
+        ' trading allowance.'
+    );
+    const baseURL =
+      ONEINCH4_API + '/' + network.id.toString() + GENERATE_ENDPOINT;
+    const url =
+      baseURL +
+      '?tokenAddress=' +
+      (fromTokenAddress || NATIVE_ADDRESS) +
+      '&amount=' +
+      txAmount;
+    fetch(url, {
+      method: 'GET',
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.groupCollapsed(
+          'RequestAllowance::Generate(tokenAddress,amount)'
+        );
+        console.log('response:', response);
+        console.groupEnd();
+        setGenerating(false);
+        setTradeTxData(response);
+        setDialog(
+          'Generated Transaction to unlock ' +
+            txAmount / 10 ** fromToken?.decimals +
+            ' ' +
+            fromTokenSymbol +
+            ' to trade.  Please sign this Tx in MetaMask.'
+        );
+        setButtonText('Sending Unlock Tx for signature...');
+      })
+      .catch((error) => {
+        console.groupCollapsed('RequestAllowance::Generate()::catch(error):');
+        console.log('Error:', error);
+        console.groupEnd();
+        setAllowance(0);
+        setGenerating(false);
+      });
 
-  useEffect(() => {
-    if (getError) {
-      setDialog(
-        `Checking ${fromTokenSymbol} trading allowance went wrong: ` +
-          getError.message
-      );
-      setButtonText('Check error...');
-      setMode('error');
-    }
-  }, [getError, fromTokenSymbol, setDialog, setButtonText, setMode]);
-
-  useEffect(() => {
-    if (forgeError) {
-      setDialog(
-        `Setting ${fromTokenSymbol} trading allowance to ${txAmount} went wrong: ` +
-          forgeError.message
-      );
-      setButtonText('Set error...');
-      setMode('error');
-    }
-  }, [
-    forgeError,
-    fromTokenSymbol,
-    setButtonText,
-    setDialog,
-    setMode,
-    txAmount,
-  ]);
-
-  const handleButton = (button) => {
-    if (mode === 'need2sign') {
-      pushTx({ data: Tx });
-      setMode('waiting');
-    } else {
-      console.groupCollapsed('ManageAllowance::handleButton()');
-      console.log('Pressed, but mode is:', mode);
-    }
+    // TODO: Fire transaction through MetaMask
+    console.log('SIGN ALOWANCE TRANSACTION');
+    setSetting(true);
+    console.log('tradeTxData:', tradeTxData);
+    setSetting(false);
   };
 
   return (
-    // TODO: fix this.
     <Box style={{ marginTop: 20 }}>
       <FormControl id="sendstart" fullWidth>
         <Tooltip title="Manage token trading allowance.">
           <span>
             <LoadingButton
-              disabled={mode !== 'need2sign'}
+              disabled={allowance < txAmount}
               variant={colorMode === 'light' ? 'outlined' : 'contained'}
               sx={{ boxShadow: 'var(--box-shadow)' }}
-              loading={isChecking || isForging}
-              onClick={handleButton}
+              loading={checking || generating || setting}
+              onClick={handleClick}
               className={
-                !txAmount || !toTokenSymbol
+                allowance < txAmount
                   ? 'allowance-button disable'
                   : 'allowance-button'
               }
