7f29e5cf445f1918c08d450e2a3d0233a1eea330
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2022-03-06 18:28:17 -0500

    Password confirm working....

--- website/src/containers/register/index.jsx
+++ website/src/containers/register/index.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMoralis } from 'react-moralis';
 import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import CircularProgress from '@mui/material/CircularProgress';
 import LoadingButton from '@mui/lab/LoadingButton';
 
@@ -11,23 +13,49 @@ import AuthLayout from '../../components/layouts/AuthLayout';
 const Register = () => {
   const { isAuthenticating, authError, signup } = useMoralis();
   const [password, setPassword] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState('false');
+  const [passwordCopy, setPasswordCopy] = useState('');
+  const [passMatchError, setPassMatchError] = useState('');
+  const [errMsg, setErrMsg] = useState('');
   const [email, setEmail] = useState('');
   const _email = email.trim();
   const disabled = !validateEmail(_email) || !password || isAuthenticating;
 
+  useEffect(() => {
+    console.log('passMatchError: ', passMatchError);
+    console.log('authError:', authError);
+    let errorMessage = '';
+    if (passMatchError.length > 3) {
+      errorMessage = passMatchError + ' ';
+    }
+    if (authError?.message !== undefined) {
+      errorMessage = errorMessage + authError.message;
+    }
+    setErrMsg(errorMessage);
+  }, [authError, passMatchError]);
+
   const handleRegister = (e) => {
     e.preventDefault();
     if (!disabled) {
-      signup(_email, password, _email, { usePost: true });
+      if (passwordCopy) {
+        if (password === passwordCopy) {
+          signup(_email, password, _email, { usePost: true });
+        } else {
+          setPassMatchError('Passwords do not match.');
+        }
+      } else {
+        setPassMatchError('Please confirm your password.');
+      }
     }
   };
 
+  const handleToggleVisiblity = (e) => {
+    e.preventDefault();
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
-    <AuthLayout
-      title="Register"
-      error={authError?.message}
-      onSubmit={handleRegister}
-    >
+    <AuthLayout title="Register" error={errMsg} onSubmit={handleRegister}>
       <TextField
         variant="standard"
         label="Email"
@@ -44,18 +72,17 @@ const Register = () => {
         autoComplete="current-password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        sx={{ mb: '74px' }}
+        sx={{ mb: '40px' }}
       />
       <TextField
         variant="standard"
         label="Confirm Password"
         type="password"
         autoComplete="current-password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        value={passwordCopy}
+        onChange={(e) => setPasswordCopy(e.target.value)}
         sx={{ mb: '74px' }}
       />
-
       <LoadingButton
         variant="white-round"
         type="submit"
