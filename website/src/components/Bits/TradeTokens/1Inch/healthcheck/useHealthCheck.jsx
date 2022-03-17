8970620f9cf9c1ad95000eb3334ffe1b24f04b8e
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2022-03-17 17:33:07 -0400

    Segmenting 1Inch Healthcheck done.

---
+++ website/src/components/Bits/TradeTokens/1Inch/healthcheck/useHealthCheck.jsx
@@ -0,0 +1,35 @@
+import { useNetwork } from '../../../../../contexts/networkContext';
+import { useExperts } from '../../../contexts/expertsContext';
+import { useTradeButton } from './TradeButtonContext';
+
+export const useHealthCheck = () => {
+  const { network } = useNetwork();
+  const { setDialog } = useExperts();
+  const { setButtonText } = useTradeButton();
+  const BASEURL = 'https://api.1inch.io/v4.0/';
+  const ENDPOINT = '/healthcheck';
+
+  const getHealthCheck = async () => {
+    const outputText = 'Checking ' + network + ' 1Inch router health...';
+    setDialog(outputText);
+    setButtonText('HealthCheck...');
+    console.log(outputText);
+    const url = BASEURL + network + ENDPOINT;
+    return await fetch(url, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setDialog('Healthcheck: ' + res.status + '.');
+        setButtonText(res.status);
+        return res.status;
+      })
+      .catch((error) => {
+        setDialog('Healthcheck error: ', error.message);
+        setButtonText('Retry');
+        console.log('Healthcheck error:', error);
+        return undefined;
+      });
+  };
+  return { getHealthCheck };
+};
