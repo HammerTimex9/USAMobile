d3da4adf26c106c60d9904873f6af71d52a68729
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2022-03-18 08:20:47 -0400

    useAllowance

---
+++ website/src/components/Bits/TradeTokens/1Inch/approve/useAllowanceTx.jsx
@@ -0,0 +1,46 @@
+import { useNetwork } from '../../../../../contexts/networkContext';
+import { useExperts } from '../../../contexts/expertsContext';
+import { useTradeButton } from './TradeButtonContext';
+import { useActions } from '../../../contexts/actionsContext';
+
+export const useAllowanceTx = () => {
+  const { network } = useNetwork();
+  const { setDialog } = useExperts();
+  const { setButtonText } = useTradeButton();
+  const { fromToken, txAmount } = useActions();
+  const BASEURL = 'https://api.1inch.io/v4.0/';
+  const ENDPOINT = '/approve/transaction';
+
+  const getAllowanceTx = async () => {
+    const outputText =
+      'Prepping ' + network + ' ' + fromToken.symbol + ' allowance...';
+    setDialog(outputText);
+    setButtonText('Prepping Allowance...');
+    console.log(outputText);
+    const url =
+      BASEURL +
+      network +
+      ENDPOINT +
+      '?tokenAddress=' +
+      fromToken.token_address +
+      '&amount=' +
+      txAmount;
+
+    return await fetch(url, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setDialog('Gas estimate to unlock allowance: ' + res.gasPrice + '.');
+        setButtonText('Allowance Prepped.');
+        return res;
+      })
+      .catch((error) => {
+        setDialog('AllowanceTx prep error: ', error.message);
+        setButtonText('Retry');
+        console.log('AllowanceTx prep error:', error);
+        return undefined;
+      });
+  };
+  return { getAllowanceTx };
+};
